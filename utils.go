4f5644c5118b9a57571e74c7bc7ec5bc45899cbe
nukopy <pytwbf201830@gmail.com>
2022-04-15 15:01:56 +0900

    feat: add time utils to variety layout

--- utils.go
+++ utils.go
@@ -9,19 +9,20 @@ func Max(x, y int) int {
 	return y
 }
 
-var layout = "2006-01-02 15:04:05"
+var layoutDefault = "2006-01-02 15:04:05"
+var layoutForFilename = "20060102-150405"
 
 func toJST(t time.Time) time.Time {
 	tJST := t.In(time.FixedZone("Asia/Tokyo", 9*60*60))
 	return tJST
 }
 
-func TimeToString(t time.Time) string {
+func TimeToString(t time.Time, layout string) string {
 	str := t.Format(layout)
 	return str
 }
 
-func TimeToJSTString(t time.Time) string {
+func TimeToJSTString(t time.Time, layout string) string {
 	tJST := toJST(t)
 	str := tJST.Format(layout)
 	return str
